806e33adebf236bcd474a76298836aab9a842cc4
Edgar Toll <edjopato@gmail.com>
2019-02-25 12:00:14 +0100

    test(categories): check if categories are useful

---
+++ categories.test.js
@@ -0,0 +1,13 @@
+import test from 'ava'
+
+import {getTopCategories} from './queries'
+
+import categories from './categories'
+
+for (const category of Object.keys(categories)) {
+	test(`category has enough subcategories: ${category}`, async t => {
+		const qNumber = categories[category]
+		const result = await getTopCategories(qNumber)
+		t.true(result.length >= 2)
+	})
+}
