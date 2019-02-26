7e28ccf90115ddf7b019093d5ac64a22d51827af
Edgar Toll <edjopato@gmail.com>
2019-02-26 15:23:32 +0100

    test: require more diversity

--- categories.test.js
+++ categories.test.js
@@ -8,6 +8,7 @@ for (const category of Object.keys(categories)) {
 	test(`category has enough subcategories: ${category}`, async t => {
 		const qNumber = categories[category]
 		const result = await getTopCategories(qNumber)
-		t.true(result.length >= 1)
+		t.log(result.length)
+		t.true(result.length >= 5)
 	})
 }
