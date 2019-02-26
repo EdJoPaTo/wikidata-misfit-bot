a76b29ced37c23ab2f580143387702e8e6307175
Edgar Toll <edjopato@gmail.com>
2019-02-26 15:59:34 +0100

    feat(category): replace company with enterprise
    better known international than a random japanese company

--- categories.js
+++ categories.js
@@ -1,8 +1,8 @@
 module.exports = {
 	animal: 'Q729',
-	company: 'Q783794',
 	computer: 'Q68',
 	creature: 'Q1274979',
+	enterprise: 'Q6881511',
 	fiction: 'Q14897293',
 	food: 'Q2095',
 	game: 'Q11410',
