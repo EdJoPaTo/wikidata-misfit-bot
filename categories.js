f13a4b22fb6adccffd0b1e1db5b158a57202c37d
Edgar Toll <edjopato@gmail.com>
2019-03-14 01:02:10 +0100

    feat(categories): add some fun categories

--- categories.js
+++ categories.js
@@ -1,4 +1,5 @@
 module.exports = {
+	art: 'Q735',
 	computer: 'Q68',
 	creature: 'Q1274979',
 	enterprise: 'Q6881511',
@@ -6,6 +7,8 @@ module.exports = {
 	food: 'Q2095',
 	game: 'Q11410',
 	instrument: 'Q34379',
+	motion: 'Q79782',
+	socialPhenomenon: 'Q602884',
 	spacecraft: 'Q40218',
 	vehicle: 'Q42889',
 	weapon: 'Q728'
